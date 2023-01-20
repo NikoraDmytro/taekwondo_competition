0b7aae3d21ba65bd2cd58b6ed47858be9d2f3e2e
Dmytro <dimaniko04@ukr.net>
2023-01-20 19:59:43 +0200

    Initialized client app with react. Installed some needed dependencies.

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.box}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
